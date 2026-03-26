383b206d79f0c43647a1092c85036687a1422e9b
Juan David Rodriguez Pachon <est.juanda.rodriguez@unimilitar.edu.co>
2026-03-26 10:14:44 -0500

    Agrege el lector del valor n, defini la estructura basica del nodo pero me di cuenta que no agrege el constructor de la clase

---
+++ taller21260326/src/main/java/Taller21260326.java
@@ -0,0 +1,15 @@
+import java.until.Scanner;
+
+class Nodo {
+    int dato;
+    Nodo siguiente;   
+}
+public class Taller21260326 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc. nextInt()
+        
+    }
+}
