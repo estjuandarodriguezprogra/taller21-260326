b032e5ca230758ca2af78f1ab5205c9efd6e705f
Juan David Rodriguez Pachon <est.juanda.rodriguez@unimilitar.edu.co>
2026-03-26 10:34:07 -0500

    Empece a crear la lista enlazada pero me aparece esta error

--- taller21260326/src/main/java/Taller21260326.java
+++ taller21260326/src/main/java/Taller21260326.java
@@ -2,14 +2,26 @@ import java.until.Scanner;
 
 class Nodo {
     int dato;
-    Nodo siguiente;   
+    Nodo siguiente;
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
 }
 public class Taller21260326 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int n = sc. nextInt()
+        int n = sc. nextInt();
+        
+         Nodo cabeza = null;
+         Nodo actual = null;
+         
+          for (int i = 0; i < n - 1; i++) {
+            int valor = sc.nextInt();
+            Nodo nuevo = new Nodo(valor); 
+            cabeza = nuevo;
+        
         
     }
 }
