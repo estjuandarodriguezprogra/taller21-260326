8159259fa3b53801ad722ad23bebe6c990213262
Juan Rodriguez <est.juanda.rodriguez@unimilitar.edu.com>
2026-03-31 20:54:19 -0500

    Lo organice y ya me corrio el programa al 100 como tenia que salir

--- taller21260326/src/main/java/Taller21260326.java
+++ taller21260326/src/main/java/Taller21260326.java
@@ -1,42 +1,5 @@
 
 import java.util.Scanner;
-
-class Nodo {
-    int dato;
-    Nodo siguiente;
-    public Nodo(int dato) {
-        this.dato = dato;
-        this.siguiente = null;
-    }
-}
-class ListaEnlazada {
-    Nodo cabeza;
-
-    public void insertar(int dato) {
-        Nodo nuevo = new Nodo(dato);
-
-        if (cabeza == null) {
-            cabeza = nuevo;
-        } else {
-            Nodo temp = cabeza;
-            while (temp.siguiente != null) {
-                temp = temp.siguiente;
-            }
-            temp.siguiente = nuevo;
-        }
-    }
-   public boolean buscar(int valor) {
-    Nodo temp = cabeza;
-
-    while (temp != null) {
-        if (temp.dato == valor) {
-            return true;
-        }
-        temp = temp.siguiente;
-    }
-
-    return false;
-}
 public class Taller21260326 {
 
     public static void main(String[] args) {
@@ -68,4 +31,4 @@ public class Taller21260326 {
     System.out.println("\nEl numero faltante es: " + faltante);
 }
 }
-}
+
