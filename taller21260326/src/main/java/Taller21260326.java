afda3b81358eb4098dcabcfd77725d8df9b99af7
Juan Rodriguez <est.juanda.rodriguez@unimilitar.edu.com>
2026-03-31 19:04:43 -0500

    Se detecto error en la ejecución del programa al no encontrarse la clase principal con método main, tuve que consultarle a la IA para identificar el problema y solucionarlo

--- taller21260326/src/main/java/Taller21260326.java
+++ taller21260326/src/main/java/Taller21260326.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 class Nodo {
@@ -54,5 +55,26 @@ public class Taller21260326 {
         }
     }       
 }
+}
+public class Taller21260326 {
+
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        ListaEnlazada lista = new ListaEnlazada();
+
+        for (int i = 0; i < n - 1; i++) {
+            lista.insertar(sc.nextInt());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!lista.buscar(i)) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
 }
 
